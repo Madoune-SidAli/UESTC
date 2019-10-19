20fd2cd3ae473a46097a1c6e3b260307aa4e7f16
Madoune Sid ali <madoune.sidali@gmail.com>
2019-10-19 21:14:30 +0100

    fisrt Commit

---
+++ Fondamentals Network Computing/First Homework/Hello/src/uestc/fnc/ws/hw1/Hello.java
@@ -0,0 +1,7 @@
+package uestc.fnc.ws.hw1;
+
+public class Hello {
+	public String helloName(String name){
+		return "Hello there " + name;
+	}
+}
---
+++ Fondamentals Network Computing/First Homework/HelloClient/src/uestc/fnc/ws/hw1/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Hello.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package uestc.fnc.ws.hw1;
+
+public interface Hello extends java.rmi.Remote {
+}
